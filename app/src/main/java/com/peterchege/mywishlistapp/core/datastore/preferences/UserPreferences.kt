a0449b89a305ffa99e705b612d78caf27d2bcef1
Peter Chege <peterkagure@gmail.com>
2023-03-25 16:16:27 +0300

    Run spotless

--- app/src/main/java/com/peterchege/mywishlistapp/core/datastore/preferences/UserPreferences.kt
+++ app/src/main/java/com/peterchege/mywishlistapp/core/datastore/preferences/UserPreferences.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 MyWishListApp By Peter Chege
+ * Copyright 2023 My Wishlist App By Peter Chege
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
